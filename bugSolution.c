dade30bbc03d012ea48d218313a5ae0e0bae4b73
BugHunter-X <askzenai@gmail.com>
2024-12-20 19:10:06 +0100

    Add bugSolution.c file

---
+++ bugSolution.c
@@ -0,0 +1,13 @@
+int main() {
+  int x = 10;
+  int *ptr = &x;
+  *ptr = 20;
+  printf("%d\n", x); // Output: 20
+
+  int y = 30;
+  ptr = &y; //Corrected
+  *ptr = 40;
+  printf("%d\n", x); // Output: 20
+  printf("%d\n", y); // Output: 40
+  return 0;
+}
